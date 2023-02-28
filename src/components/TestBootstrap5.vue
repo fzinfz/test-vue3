81262087ca7d7daa15b8af09e6a9abe0f2d9a1e9
Ferro <fzinfz@gmail.com>
2023-02-28 23:37:50 +0800

    mv to bs5 css & test v-bind

---
+++ src/components/TestBootstrap5.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string, src_link: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+
+  <div class="container">
+    <div class="d-flex justify-content-between">
+        <div></div>
+        <div>
+            <h4 class="text-muted">{{ msg }}</h4>
+        </div>
+        <div class="d-flex justify-content-end">
+            <a :href="src_link" class="mr-1" target="_blank">Source</a>
+        </div>
+    </div>
+</div>
+
+<form class="container">
+  <div class="form-floating pb-1">
+        <textarea class="form-control" id="input" style="height: 150px"></textarea>
+        <label for="input">Paste Input here</label>
+    </div>
+    <div>
+        <button class="btn btn-info m-1">btn-info</button>
+
+        <label class="form-check-label">
+            <input class="form-check-input mx-2" type="checkbox" id="is_center" value="">form-check-label
+        </label>
+      </div>
+      <div class="form-floating pt-1">
+        <textarea class="form-control" id="output" style="height: 500px"></textarea>
+        <label for="output">Result will be shown here</label>
+      </div>
+</form>
+</template>
