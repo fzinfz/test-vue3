2b6e61c127db50b84fccbc5e21f3a4f9b1dc9c5e
Ferro <fzinfz@gmail.com>
2023-03-08 01:06:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Table4Links from './components/Table4Links.vue';
+</script>
+
+<template>
+  <Table4Links msg="test"></Table4Links>
+</template>
---
+++ src/components/Table4Links.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <div class="text-3xl font-bold underline">{{ msg }}</div>
+  </div>
+</template>
