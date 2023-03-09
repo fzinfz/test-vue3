5efbbcbca3f26c5c574bb8f7d5489b030339563b
Ferro <fzinfz@gmail.com>
2023-03-10 00:59:14 +0800

    test quasar/vite-plugin

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,195 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'protein', label: 'Protein (g)', field: 'protein' },
+  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+  { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: '14%',
+    iron: '1%'
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: '8%',
+    iron: '1%'
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: '6%',
+    iron: '7%'
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    sodium: 413,
+    calcium: '3%',
+    iron: '8%'
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    sodium: 327,
+    calcium: '7%',
+    iron: '16%'
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    sodium: 50,
+    calcium: '0%',
+    iron: '0%'
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    sodium: 38,
+    calcium: '0%',
+    iron: '2%'
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    sodium: 562,
+    calcium: '0%',
+    iron: '45%'
+  },
+  {
+    name: 'Donut',
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    sodium: 326,
+    calcium: '2%',
+    iron: '22%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  }
+]
+
+export default {
+  setup () {
+    return {
+      columns,
+      rows
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="q-pa-md q-gutter-sm">
+    <q-btn color="white" text-color="black" label="Standard" />
+    <q-btn color="primary" label="Primary" />
+    <q-btn color="secondary" label="Secondary" />
+    <q-btn color="amber" glossy label="Amber" />
+    <q-btn color="brown-5" label="Brown 5" />
+    <q-btn color="deep-orange" glossy label="Deep Orange" />
+    <q-btn color="purple" label="Purple" />
+    <q-btn color="black" label="Black" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="q-pa-md q-gutter-y-md column items-start">
+    <q-btn-group push>
+      <q-btn push label="First" icon="timeline" />
+      <q-btn push label="Second" icon="visibility" />
+      <q-btn push label="Third" icon="update" />
+    </q-btn-group>
+
+    <q-btn-group push>
+      <q-btn color="yellow" glossy text-color="black" push label="First" icon="verified_user" />
+      <q-btn color="amber" glossy text-color="black" push label="Second" />
+      <q-btn color="orange" glossy text-color="black" push label="Third" />
+    </q-btn-group>
+
+    <q-btn-group outline>
+      <q-btn outline color="brown" label="First" />
+      <q-btn outline color="brown" label="Second" icon-right="watch_later" />
+      <q-btn outline color="brown" label="Third" />
+    </q-btn-group>
+
+    <q-btn-group>
+      <q-btn color="secondary" glossy label="First" />
+      <q-btn color="secondary" glossy label="Second" />
+      <q-btn color="secondary" glossy label="Third" />
+      <q-btn color="secondary" glossy label="Fourth" />
+    </q-btn-group>
+
+    <q-btn-group>
+      <q-btn color="accent" icon="timeline" />
+      <q-btn color="accent" icon="visibility" />
+      <q-btn color="accent" icon="update" />
+    </q-btn-group>
+
+    <q-btn-group rounded>
+      <q-btn color="amber" rounded glossy icon="timeline" />
+      <q-btn color="amber" rounded glossy icon="visibility" />
+      <q-btn color="amber" rounded glossy icon-right="update" label="Update" />
+    </q-btn-group>
+  </div>
+  <div class="q-pa-md">
+    <q-table
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+    />
+  </div>
+  </div>
 </template>
 
 <style scoped>
